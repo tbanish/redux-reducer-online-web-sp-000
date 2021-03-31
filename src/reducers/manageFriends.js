e3322810a5acf8b5b71af30af27a46125c11cf58
tbanish <tombanish@gmail.com>
2021-03-31 12:34:22 -0400

    commit tree 3ae8b1a21c10d95cbe4488cefb0659ca88b10c2b

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      const addFriendsArray = state.friends
+      addFriendsArray.push(action.friend)
+      return {...state, friends: addFriendsArray}
+    case 'REMOVE_FRIEND':
+      const friends = state.friends.filter(friend => friend.id !== action.id)
+      let newState = {...state, friends: friends}
+      return newState
+    default:
+      return state
+  }
 }
